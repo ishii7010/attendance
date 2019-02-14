1b03b64d18117164695538ddd3df7453f38912a7
n.ishii <n.ishii@adseed.co.jp>
2019-02-14 10:55:59 +0900

    work_time

--- app/Http/Controllers/worksController.php
+++ app/Http/Controllers/worksController.php
@@ -3,8 +3,49 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Work;
+use Carbon\Carbon;
 
 class worksController extends Controller
 {
-    //
+    public function create()
+    {
+        $user = \Auth::user();
+
+        if ( $user->status == 1 )
+        {
+            $work = $user->works->last();
+        }
+        return view('work.create', compact('user', 'work'));
+    }
+
+    public function store()
+    {
+        $user = \Auth::user();
+        $work = new Work;
+        $work->user_id = $user->id;
+        $work->start_time = Carbon::now();
+        if ($work->save())
+        {
+            $user->update(['status' => 1]);
+            return redirect('work/create');
+        }
+        else {
+            return redirect('work/create');
+        }
+    }
+
+    public function update(Request $request, Work $work)
+    {
+        $user = \Auth::user();
+        $work->end_time = Carbon::now();
+        if ($work->save())
+        {
+            $user->update(['status' => 0]);
+            return redirect('work/create');
+        }
+        else {
+            return redirect('work/create');
+        }
+    }
 }
--- database/migrations/2019_02_13_033628_create_works_table.php
+++ database/migrations/2019_02_13_033628_create_works_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateWorkTable extends Migration
+class CreateWorksTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,11 @@ class CreateWorkTable extends Migration
      */
     public function up()
     {
-        Schema::create('work', function (Blueprint $table) {
+        Schema::create('works', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('user_id');
             $table->dateTime('start_time');
-            $table->dateTime('end_time');
+            $table->dateTime('end_time')->nullable();
             $table->timestamps();
         });
     }
@@ -29,6 +29,6 @@ class CreateWorkTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('work');
+        Schema::dropIfExists('works');
     }
 }
